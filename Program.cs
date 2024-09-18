36f6f22f77ebef04ca6b64c266898ff0d1444a9c
Jacen <yjshu2017@gmail.com>
2024-09-18 17:30:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿/* 
+ * should we even bother with header comments lol
+ * mehhh
+ * 
+ */
+
+internal class Program {
+    private static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+    }
+}
